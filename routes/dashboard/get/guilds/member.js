ca4f3c54d30058e5d51df75c46614ecdb35497ba
NotPiny <103984634+NotPiny@users.noreply.github.com>
2023-10-01 13:21:25 +0100

    Add caching to member and user routes

--- routes/dashboard/get/guilds/member.js
+++ routes/dashboard/get/guilds/member.js
@@ -1,5 +1,6 @@
-const express = require('express');
-const client = require('../../../../client.js');
+const express = require('express')
+const client = require('../../../../client.js')
+let members = [] // Cache members until the api restarts or something idk
 
 /**
  * @param {express.Request} req
@@ -11,11 +12,18 @@ module.exports = (req, res) => {
     const member = req.query.member
     if (!member) return res.json({ error: 'No member provided' })
 
-    const guildObj = client.guilds.cache.get(guild)
-    if (!guildObj) return res.json({ error: 'Invalid guild' })
+    // Check if the member for the guild is cached so we don't have to make discord mad by spamming requests
+    const cachedMember = members.find(m => m.userId === member && m.guildId === guild)
+    if (cachedMember) {
+        return res.status(200).send(cachedMember)
+    } else {
+        const memberObj = client.guilds.cache.get(guild).members.cache.get(member)
+        if (!memberObj) return res.status(400).json({ error: 'Invalid member' })
 
-    const memberObj = guildObj.members.cache.get(member)
-    if (!memberObj) return res.json({ error: 'Invalid member' })
+        members.push(memberObj)
+        const membersString = JSON.stringify(members);
+        members = JSON.parse(membersString); // Hacky way to make cache work because .push() doesn't work for some reason
 
-    res.status(200).send(memberObj)
+        res.status(200).send(memberObj)
+    }
 }
--- routes/dashboard/get/users/user.js
+++ routes/dashboard/get/users/user.js
@@ -1,18 +1,28 @@
 const express = require('express');
 const client = require('../../../../client.js');
+let users = [] // Cache users until the api restarts or something idk (bad idea for ram but ill fix that if it becomes a problem)
 
 /**
  * @param {express.Request} req
  * @param {express.Response} res
 */
 module.exports = (req, res) => {
-    const guild = req.query.guild
-
     const user = req.query.user
     if (!user) return res.json({ error: 'No user provided' })
 
-    const userObj = client.users.cache.get(user)
-    if (!userObj) return res.json({ error: 'Invalid user' })
+    // Check if the user is cached so we don't have to make discord mad by spamming requests
+    const cachedUser = users.find(u => u.id === user)
 
-    res.status(200).send(userObj)
+    if (cachedUser) {
+        return res.status(200).send(cachedUser)
+    } else {
+        const userObj = client.users.cache.get(user)
+        if (!userObj) return res.status(400).json({ error: 'Invalid user' })
+
+        users.push(userObj)
+        const usersString = JSON.stringify(users);
+        users = JSON.parse(usersString); // Hacky way to make cache work because .push() doesn't work for some reason
+
+        res.status(200).send(userObj)
+    }
 }
